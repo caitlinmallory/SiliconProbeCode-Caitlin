a2b1f85ab9798bbcc436055a41085d4c7f882c72
caitlinmallory <sc8lin@gmail.com>
2019-11-15 14:34:05 -0800

    changed timestamps

--- neuralynx2kilosortSherlock.m
+++ neuralynx2kilosortSherlock.m
@@ -28,10 +28,10 @@ function filename = neuralynx2kilosortSherlock(directoryPath, target_dir)
     
     fprintf(strcat('\nStart Processing: ', datestr(now,'mmmm dd, yyyy HH:MM:SS AM'),'\n'));
     numOfChannels = 64;
-    timeStamps = '6159665707_7893828980';
+    timeStamps = '18794456157_18990710047';
         
     for csc = 1:numOfChannels
-        gi
+       
         cscPath = fullfile(directoryPath, ['CSC' num2str(csc) '_' timeStamps '.ncs']);
 
         % load neuralynx file, linearize samples, convert to int16
