3a82e0486bf8a959cfac03cb3ae247da7df116b7
caitlinmallory <sc8lin@gmail.com>
2019-11-21 17:03:32 -0800

    m

--- convertNL2binScript.m
+++ convertNL2binScript.m
@@ -1,5 +1,5 @@
 addpath('/home/users/cmallory/SiliconProbeCode-Caitlin')
-%addpath('/home/users/cmallory/SiliconProbeCode-Caitlin/MatlabImportExport_v6.0.0')
+addpath('/home/users/cmallory/SiliconProbeCode-Caitlin/MatlabImportExport_v6.0.0')
 addpath('/home/users/cmallory/SiliconProbeCode-Caitlin/releaseDec2015')
 addpath('/home/users/cmallory/SiliconProbeCode-Caitlin/releaseDec2015/binaries')
 addpath('/home/users/cmallory/SiliconProbeCode-Caitlin/releaseDec2015/source')
